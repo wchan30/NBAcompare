1b2cdffbbd4852923d15abe9fd7e992c6130e3dc
wchan30 <103913122+wchan30@users.noreply.github.com>
2024-01-04 21:42:39 -0500

    util.py

--- util.py
+++ util.py
@@ -5,4 +5,46 @@ import pandas as pd
 def lifespan(player_id):
     career = playercareerstats.PlayerCareerStats(player_id=player_id)
     return career.get_data_frames()[0]
+
+def totalGames(player_id):
+    career = playercareerstats.PlayerCareerStats(player_id=player_id)
+    career_df = career.get_data_frames()[0]
+    total_games = career_df['GP'].sum()
+    return total_games
+
+def threePointAttempt(player_id):
+    career = playercareerstats.PlayerCareerStats(player_id=player_id)
+    career_df = career.get_data_frames()[0]
+    three_points_attempt = career_df['FG3A'].sum()
+    return three_points_attempt
+
+def fieldGoalAttempt(player_id):
+    career = playercareerstats.PlayerCareerStats(player_id=player_id)
+    career_df = career.get_data_frames()[0]
+    field_goal_attempt = career_df['FGA'].sum()
+    return field_goal_attempt
+
+def freeThrowAttempt(player_id):
+    career = playercareerstats.PlayerCareerStats(player_id=player_id)
+    career_df = career.get_data_frames()[0]
+    field_goal_attempt = career_df['FTA'].sum()
+    return field_goal_attempt
+
+def pointsPerGame(player_id):
+    career = playercareerstats.PlayerCareerStats(player_id=player_id)
+    career_df = career.get_data_frames()[0]
+    points_per_game = career_df['PTS'].sum()
+    return points_per_game
+
+def assistsPerGame(player_id):
+    career = playercareerstats.PlayerCareerStats(player_id=player_id)
+    career_df = career.get_data_frames()[0]
+    assists_per_game = career_df['AST'].sum()
+    return assists_per_game
+
+def reboundsPerGame(player_id):
+    career = playercareerstats.PlayerCareerStats(player_id=player_id)
+    career_df = career.get_data_frames()[0]
+    rebounds_per_game = career_df['REB'].sum()
+    return rebounds_per_game
     
